3c9b13e5a3277b1843a8836086323b8ce5571859
Marcus Brummer <mbrlabs7@gmail.com>
2020-09-15 14:20:36 +0200

    Level design: castle outline

--- game/Props/Grass/Grass.gd
+++ game/Props/Grass/Grass.gd
@@ -1,5 +1,5 @@
 tool
-extends MeshInstance
+extends StaticBody
 
 const A = [0.0, 0.0]
 const B = [0.0, 0.5]
@@ -8,29 +8,35 @@ const D = [0.5, 0.5]
 const UV_MAP = [B, A, A, A, A, A, C, C, C, D, D, D]
 const PI2 = 6.28319
 
-onready var mm: MultiMeshInstance = $GrassMultiMesh
-export var grass_count: int = 128
+onready var _mm: MultiMeshInstance = $Chunk/GrassMultiMesh
+onready var _base_mesh: MeshInstance = $Chunk
+
+export var grass_count: int = 200
 
 func _ready():
-	mm.multimesh.instance_count = grass_count
-	mm.multimesh.visible_instance_count = grass_count
+	_mm.multimesh = MultiMesh.new()
+	_mm.multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	_mm.multimesh.custom_data_format = MultiMesh.CUSTOM_DATA_FLOAT
+	_mm.multimesh.instance_count = grass_count
+	_mm.multimesh.mesh = preload("res://Props/Grass/grass.obj")
+	_mm.multimesh.visible_instance_count  = grass_count
 	
-	var aabb_size = get_aabb().size
-	var pos = mm.transform.origin
+	var aabb_size = _base_mesh.get_aabb().size
+	var pos = _mm.transform.origin
 	
 	var min_x = pos.x - (aabb_size.x / 2.0)
 	var max_x = pos.x + (aabb_size.x / 2.0)
 	var min_z = pos.z + (aabb_size.z / 2.0)
 	var max_z = pos.z - (aabb_size.z / 2.0)
 	
-	for i in range(grass_count):
+	for i in grass_count:
 		var x = rand_range(min_x, max_x)
 		var z = rand_range(min_z, max_z)
 		var scl = rand_range(0.66, 1.0)
 		var uvs := int(rand_range(0, UV_MAP.size()))
-		var basis = mm.transform.scaled(Vector3(scl, scl, scl)).rotated(Vector3.UP, rand_range(-1, 1)*PI2).basis
-		mm.multimesh.set_instance_transform(i, Transform(basis, Vector3(x, 0, z)))
-		mm.multimesh.set_instance_custom_data(i, Color(
+		var basis = _mm.transform.scaled(Vector3(scl, scl, scl)).rotated(Vector3.UP, rand_range(-1, 1)*PI2).basis
+		_mm.multimesh.set_instance_transform(i, Transform(basis, Vector3(x, 0, z)))
+		_mm.multimesh.set_instance_custom_data(i, Color(
 			0.5+randf()*0.5, # random value; used for per-instance-randomness on x-axis wind movement
 			0.5+randf()*0.5, # random value; used for per-instance-randomness on z-axis wind movement
 			UV_MAP[uvs][0],	 # x-UV coordinate
