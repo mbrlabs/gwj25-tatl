d6f7f2019773b369bde94867065a1a2401145ed1
Marcus Brummer <mbrlabs7@gmail.com>
2020-09-20 12:08:28 +0200

    Polish

--- game/Interactables/GodotCrypt.gd
+++ game/Interactables/GodotCrypt.gd
@@ -4,6 +4,7 @@ extends Interactable
 onready var _head: GodotHead = $GodotHead
 onready var _godot_talk_sound: AudioStreamPlayer = $GodotTalk
 onready var _camera_shake_timer: Timer = $CameraShakeTimer
+onready var _particles: CPUParticles = $GodotHead/CPUParticles
 
 # ---------------------------------------------------------------------------------------
 const T = "Tatl:"
@@ -11,12 +12,12 @@ const C = "Spirit of Godot:"
 var _dialogs = [
 	[T, "Hmm, that thing looks important. I wonder what happens if i touch that... ARGHHHH, IT MOVED!"],
 	[C, "What is this? Who disturbes my sleep? Oh..you are a faerie. Forgive me my manners, i am the Spirit of Godot. What are you doing this far away from home?"],
-	[T, "I don't know. I'm completly lost :("],
+	[T, "I don't know, i'm completly lost! One moment i was at home and in the other i found myself here..."],
 	[C, "Indeed, this is the Forest of Darkness where the light never shines...no place for a fearie. But i will help you! I grant you the mighty force of the Godot-Cannon."],
 	[C, "You will have to transform yourself into the Godot-Form to be able to use it. Press the [Right Mouse Button] to start the transformation."],
 	[C, "Great. Now you can use the Godot-Cannon with the [Left Mouse Button]. Note however, that this form also makes you slower."],
 	[C, "To transform back, simply press the [Right Mouse Button] again."],
-	[T, "Wow, thank you so much!"],
+	[T, "Wooooo, thank you so much! I guess this will come in handy."],
 	[C, "You're welcome little faerie. I still have to warn you though: there a beings in this world, that can be extremly dangerous. Normally they are quite peacful, but..."],
 	[C, "DO NOT try to talk to them or even attack them...they don't like that. I will go back to sleep now. Good luck to you."],
 ]
@@ -102,3 +103,13 @@ func _on_CrowSoundTimer2_timeout():
 # ---------------------------------------------------------------------------------------
 func _on_CameraShakeTimer_timeout():
 	get_viewport().get_camera().add_trauma(0.5)
+
+# ---------------------------------------------------------------------------------------
+func _on_ParticlesActivationArea_body_entered(body: PhysicsBody):
+	if body.collision_layer == 1:
+		_particles.emitting = true
+		
+# ---------------------------------------------------------------------------------------
+func _on_ParticlesActivationArea_body_exited(body: PhysicsBody):
+	if body.collision_layer == 1:
+		_particles.emitting = false
---
+++ game/POIs/POIRoadBlock.gd
@@ -0,0 +1,13 @@
+extends POIArea
+
+# ---------------------------------------------------------------------------------------
+func _is_triggerable() -> bool:
+	return true
+
+# ---------------------------------------------------------------------------------------
+func _get_dialog_title() -> String:
+	return "Tatl:"
+
+# ---------------------------------------------------------------------------------------
+func _get_dialog_message() -> String:
+	return "Wow, there is a path into the montain. But it's blocked..."
